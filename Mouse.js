e8ee15747757b43e71e05ef1251e5d7c0458cff8
tonganh <tongngocanhcampha@gmail.com>
2020-08-09 09:00:39 +0700

    Revert "Add die method for mouse"
    This reverts commit 2429a7cee0f682a83a353a637cf5dc6b914d0b20.

--- Mouse.js
+++ Mouse.js
@@ -1,8 +1,4 @@
-function Mouse(name) {
-  this.name = name;
-  this.dead = false;
+function Mouse(name){
+    this.name=name;
 };
-Mouse.prototype.die = function() {
-  this.dead = true;
-};
-module.exports = Mouse;
+module.exports=Mouse;
