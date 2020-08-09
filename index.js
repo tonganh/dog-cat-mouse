ffddbe298b37dc1e852de9db831b840e578e1bca
tonganh <tongngocanhcampha@gmail.com>
2020-08-09 14:06:46 +0700

    Impove cat eat method

---
+++ index.js
@@ -0,0 +1,13 @@
+var Cat = require('./Cat');
+var Mouse = require('./Mouse');
+var Dog = require('./Dog');
+var cat = new Cat();
+var mouse = new Mouse('Mickey');
+var dog = new Dog('Ki');
+cat.eat(mouse);
+try {
+  cat.eat(dog);
+} catch (error) {
+  console.log('Error while cat eat dog!');
+}
+console.log(cat);
