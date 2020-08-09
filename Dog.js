d5459598f06a956c950166253a5a0d311cb17dcd
tonganh <tongngocanhcampha@gmail.com>
2020-08-09 10:08:22 +0700

    Say hi method for dog

--- Dog.js
+++ Dog.js
@@ -1,6 +1,12 @@
-function Dog() {
+var chalk = require('chalk');
+function Dog(name) {
+  this.name = name;
   this.stomach = [];
 }
 Dog.prototype.eat = function(Cat) {
   this.stomach.push(Cat);
-}
+};
+Dog.prototype.sayHi = function() {
+  console.log('Hi my name is  ' + chalk.redBright(this.name));
+};
+module.exports = Dog;
